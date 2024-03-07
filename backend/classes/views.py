73688e8d6e82ad1784b3d245f53cdb10c8f52cb2
Richardson Christian <rcsc3w@yahoo.com.br>
2024-03-07 12:29:57 -0300

    removido method POST

--- backend/classes/views.py
+++ backend/classes/views.py
@@ -7,18 +7,9 @@ from classes.models import Classes
 from classes.setializer import ClassesSerializer
 
 
-@api_view(["GET", "POST"])
+@api_view(["GET"])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def classes(request):
-    if request.method == 'POST':
-        if request.user.is_superuser:
-            body = request.data
-            serializer = ClassesSerializer(data=body)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response({'message': serializer.errors['nome']}, status=status.HTTP_409_CONFLICT)
     all = Classes.objects.all()
     serializer = ClassesSerializer(all, many=True)
     return Response(serializer.data)
