e52d9bd53e243bf66b56444b3e20394b3f8e92a8
Richardson Christian <rcsc3w@yahoo.com.br>
2024-03-08 22:39:33 -0300

    campo para path de imagem criado

--- backend/classes/models.py
+++ backend/classes/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class Classes(models.Model):
+    idClasse = models.PositiveIntegerField(default=0)
     nome = models.CharField(max_length=255, default="", unique=True)
+    img_path = models.CharField(max_length=255, default="")
+
+    def __str__(self) -> str:
+        return self.nome
 
     class Meta:
         db_table = "classes"
